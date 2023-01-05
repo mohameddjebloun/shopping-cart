1af1abc74e48608d0b069cc14a77c068823cdaa0
mohameddjebloun <djeblounm@gmail.com>
2023-01-05 19:09:40 +0100

    Create the navigation logic

--- src/App.js
+++ src/App.js
@@ -1,5 +1,25 @@
+import React from "react";
+import { Route, Link, Routes } from "react-router-dom";
+import Homepage from "./pages/Homepage";
+import Shop from "./pages/Shop";
+
 function App() {
-  return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
+  return (
+    <React.Fragment>
+      <nav className="navbar bg-base-100 flex justify-center">
+        <Link className="btn btn-ghost normal-case text-xl" to="/">
+          Home
+        </Link>
+        <Link className="btn btn-ghost normal-case text-xl" to="/shop">
+          Shop
+        </Link>
+      </nav>
+      <Routes>
+        <Route path="*" element={<Homepage />} />
+        <Route path="/shop" element={<Shop />} />
+      </Routes>
+    </React.Fragment>
+  );
 }
 
 export default App;
