1277b4e28b725c3988536a1330401f2c9b6d85ca
mohameddjebloun <djeblounm@gmail.com>
2023-01-10 18:45:53 +0100

    Add Cart component

--- src/App.js
+++ src/App.js
@@ -2,10 +2,13 @@ import React from "react";
 import { Route, Link, Routes } from "react-router-dom";
 import Homepage from "./pages/Homepage";
 import Shop from "./pages/Shop";
+import CartButton from "./components/CartButton";
+import Cart from "./components/Cart/Cart";
 
 function App() {
   return (
     <React.Fragment>
+      <Cart />
       <nav className="navbar bg-base-100 flex justify-center top-0 z-50 h-fit w-full sticky">
         <Link className="btn btn-ghost normal-case text-xl" to="/">
           Home
@@ -13,6 +16,7 @@ function App() {
         <Link className="btn btn-ghost normal-case text-xl" to="/shop">
           Shop
         </Link>
+        <CartButton />
       </nav>
       <Routes>
         <Route path="*" element={<Homepage />} />
