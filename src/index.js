0174f54bde516db8a929eea1cdbc4ae78d1666c3
mohameddjebloun <djeblounm@gmail.com>
2023-01-10 21:57:30 +0100

    Add basename

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ import store from "./store/store";
 import { Provider } from "react-redux";
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <BrowserRouter>
+  <BrowserRouter basename="/app">
     <Provider store={store}>
       <App />
     </Provider>
