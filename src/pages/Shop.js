f51404af59a83b266cc179aa24c10b4ff5118ebd
mohameddjebloun <djeblounm@gmail.com>
2023-01-10 12:19:55 +0100

    Create the Shop page

---
+++ src/pages/Shop.js
@@ -0,0 +1,37 @@
+import ProductCard from "../components/ProductCard";
+import products from "../clothes.json";
+import CategoriesRadio from "../components/CategoriesRadio";
+import { useState } from "react";
+
+const Shop = () => {
+  //Handle the categories filtering logic
+  const [selectedCategory, setSelectedCategory] = useState("All");
+  const categories = Array.from(new Set(products.map((item) => item.category)));
+  const filteredProducts =
+    selectedCategory === "All"
+      ? products
+      : products.filter((product) => product.category === selectedCategory);
+  return (
+    <>
+      <CategoriesRadio
+        selectedCategory={selectedCategory}
+        setSelectedCategory={setSelectedCategory}
+        categories={categories}
+      />
+      <div className="pt-8 flex flex-wrap gap-1 justify-center content-center">
+        {products &&
+          filteredProducts.map((product) => {
+            return (
+              <ProductCard
+                key={product.id}
+                name={product.name}
+                price={product.price}
+              />
+            );
+          })}
+      </div>
+    </>
+  );
+};
+
+export default Shop;
